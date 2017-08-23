35b6102e58901b3f5ac9c4ef0460b36f3018d7cd
Adapa Janardhana Swamy <haijanardhan@gmail.com>
2017-08-23 11:02:09 +0530

    Add files via upload

--- agent.py
+++ agent.py
@@ -148,7 +148,8 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
-        self.Q[state][action] = (1 - self.alpha) * self.Q[state][action] + self.alpha * reward
+        if(self.learning==True)
+            self.Q[state][action] = (1 - self.alpha) * self.Q[state][action] + self.alpha * reward
         return
 
 
